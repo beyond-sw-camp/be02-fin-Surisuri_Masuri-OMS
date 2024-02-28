63046ee16cc13adc713bf4218fe146d6e51db63e
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-02-28 10:05:46 +0900

    [init] 1st commit

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/SurisuriMasuriApplication.java
@@ -0,0 +1,13 @@
+package com.example.Surisuri_Masuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurisuriMasuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurisuriMasuriApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Surisuri_Masuri/SurisuriMasuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Surisuri_Masuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurisuriMasuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
