ea47e04daf8ea5c646c9a89c8e135bb80b88f097
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-08 14:48:42 +0900

    [Feat] CustomExceptionHandler 추가
    - CONSTRAINT_VIOLATION 관련 ExceptionHandler 추가
    - INTERNAL_SERVER_ERROR 관련 ExceptionHandler 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
-package backend.src.main.java.com.example.Surisuri_Masuri.exception;
+package com.example.Surisuri_Masuri.exception;
 
-import common.BaseResponse;
+import com.example.Surisuri_Masuri.common.BaseResponse;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,5 +12,19 @@ public class CustomExceptionHandler {
     protected ResponseEntity<Object> handleCustomException(CustomException e){
         return ResponseEntity.status(e.getErrorCode().getStatus())
                 .body(BaseResponse.failResponse(e.getErrorCode().getCode(), e.getErrorCode().name()));
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e) {
+        ErrorCode errorCode = ErrorCode.CONSTRAINT_VIOLATION;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
     }
 }
