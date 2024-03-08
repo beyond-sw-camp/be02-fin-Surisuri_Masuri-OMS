3ba71c7924208cab7b11d5a55b6056259a80093d
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-08 15:55:00 +0900

    [Fix] CustomException 수정
    - 코드 수정 후 push를 위한 주석 처리

--- backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomException.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomException.java
@@ -2,31 +2,9 @@ package com.example.Surisuri_Masuri.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+/*
 @Getter
-@AllArgsConstructor
 public class CustomException extends RuntimeException {
-    private ErrorCode errorCode;
-    private String message;
-    private Integer code;
 
-    public CustomException(ErrorCode errorCode) {
-        this.errorCode = errorCode;
-        this.message = null;
-        this.code = null;
-    }
-
-    public CustomException(ErrorCode errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message;
-    }
-
-
-    public String getMessage() {
-        if (message == null) {
-            return errorCode.getMessage();
-        }
-
-        return String.format("%s. %s", errorCode.getMessage(), message);
-    }
 }
+ */
