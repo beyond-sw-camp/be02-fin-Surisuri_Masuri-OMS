a567703cd32d3ab0acf578c0e47270fa0c113103
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-08 16:08:22 +0900

    [Fix] CustomExceptionHandler 수정
    - handleCustomException 메소드 삭제

--- backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomExceptionHandler.java
@@ -8,23 +8,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<Object> handleCustomException(CustomException e){
-        return ResponseEntity.status(e.getErrorCode().getStatus())
-                .body(BaseResponse.failResponse(e.getErrorCode().getCode(), e.getErrorCode().name()));
-    }
 
-    @ExceptionHandler(CustomException.class)
-    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException e) {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
         ErrorCode errorCode = ErrorCode.CONSTRAINT_VIOLATION;
         ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
-        return new ResponseEntity<>(response, errorCode.getStatus());
+        return ResponseEntity.ok().body(response);
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
         ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
-        return new ResponseEntity<>(response, errorCode.getStatus());
+        return ResponseEntity.ok().body(response);
     }
 }
