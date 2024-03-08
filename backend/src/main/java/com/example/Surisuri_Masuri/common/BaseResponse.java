0db75cce63cce10c96334c459cf52f98cde657ae
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-08 14:39:09 +0900

    [Fix] package 경로 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/common/BaseResponse.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/common/BaseResponse.java
@@ -1,4 +1,4 @@
-package backend.src.main.java.com.example.Surisuri_Masuri.common;
+package com.example.Surisuri_Masuri.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomException.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/CustomException.java
@@ -1,4 +1,4 @@
-package backend.src.main.java.com.example.Surisuri_Masuri.exception;
+package com.example.Surisuri_Masuri.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
