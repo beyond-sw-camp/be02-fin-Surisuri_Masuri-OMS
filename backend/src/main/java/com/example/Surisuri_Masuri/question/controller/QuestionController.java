d25cd62d03672a14a2228765376caf224a6ab0c7
jwj3 <llongkate@naver.com>
2024-03-11 11:29:32 +0900

    [Feat] 문의사항 기능 개발
    문의사항 작성 기능,문의사항 조회 기능, 문의사항 수정 기능, 문의사항 삭제 기능을 구현했습니다.
    
    #15 #16 #17 #18

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/question/controller/QuestionController.java
@@ -0,0 +1,50 @@
+package com.example.Surisuri_Masuri.question.controller;
+
+import com.example.Surisuri_Masuri.common.BaseResponse;
+import com.example.Surisuri_Masuri.question.model.request.PatchUpdateQuestionReq;
+import com.example.Surisuri_Masuri.question.model.request.PostCreateQuestionReq;
+import com.example.Surisuri_Masuri.question.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/question")
+@CrossOrigin("*")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/create")
+    public ResponseEntity create(PostCreateQuestionReq postCreateQuestionReq) {
+        BaseResponse baseResponse = questionService.create(postCreateQuestionReq);
+        return ResponseEntity.ok().body(baseResponse);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
+    public ResponseEntity list(Integer page, Integer size) {
+        BaseResponse baseResponse = questionService.list(page,size);
+        return ResponseEntity.ok().body(baseResponse);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "/update")
+    public ResponseEntity update(PatchUpdateQuestionReq patchUpdateQuestionReq) {
+        BaseResponse baseResponse = questionService.update(patchUpdateQuestionReq);
+        return ResponseEntity.ok().body(baseResponse);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
+    public ResponseEntity delete(Integer questionIdx) {
+        BaseResponse baseResponse = questionService.delete(questionIdx);
+        return ResponseEntity.ok().body(baseResponse);
+    }
+
+    // TODO: 2024-03-10 관리자가 답변을 작성하는 기능 추가해야함
+    @RequestMapping(method = RequestMethod.POST, value = "/answer")
+    public ResponseEntity answer() {
+        return ResponseEntity.ok().body("");
+    }
+
+}
---
+++ backend/src/main/java/com/example/Surisuri_Masuri/question/model/entity/Question.java
@@ -0,0 +1,71 @@
+package com.example.Surisuri_Masuri.question.model.entity;
+
+import com.example.Surisuri_Masuri.question.model.request.PatchUpdateQuestionReq;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    // TODO: 2024-03-10 user 테이블과 관계 맺어야 함 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer questionIdx;
+
+    @Column(nullable = false, length = 50)
+    private String category;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @Column(nullable = false, length = 1)
+    private Boolean status;
+
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    private Date updatedAt;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = Timestamp.from(Instant.now());
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public void update(PatchUpdateQuestionReq patchUpdateQuestionReq) {
+        if (patchUpdateQuestionReq.getCategory() != null) {
+            this.category = patchUpdateQuestionReq.getCategory();
+        }
+        if (patchUpdateQuestionReq.getTitle() != null) {
+            this.title = patchUpdateQuestionReq.getTitle();
+        }
+        if (patchUpdateQuestionReq.getContent() != null) {
+            this.content = patchUpdateQuestionReq.getContent();
+        }
+        if (patchUpdateQuestionReq.getStatus() != null) {
+            this.status = patchUpdateQuestionReq.getStatus();
+        }
+    }
+}
+
