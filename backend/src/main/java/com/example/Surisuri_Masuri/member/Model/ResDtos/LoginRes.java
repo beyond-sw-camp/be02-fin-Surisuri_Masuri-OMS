ea805928661dee181996934304d60eff7597cebb
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-11 12:16:19 +0900

    [Feat] Login 관련 Dto 구현
      - LoginRes 작성
      - LoginReq 작성

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ResDtos/LoginRes.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ResDtos/LoginRes.java
@@ -1,4 +1,10 @@
 package com.example.Surisuri_Masuri.member.Model.ResDtos;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class LoginRes {
+    private final String jwtToken;
 }
