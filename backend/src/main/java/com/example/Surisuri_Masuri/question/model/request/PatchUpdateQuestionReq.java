22f7216b3be48d3d9297a19743586a24959a6ccf
jwj3 <llongkate@naver.com>
2024-03-11 11:31:35 +0900

    [Feat] 문의사항 수정 DTO 추가
    문의사항 수정 기능에 대한 요청 DTO를 추가했습니다.
    
    #17

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/question/model/request/PatchUpdateQuestionReq.java
@@ -0,0 +1,20 @@
+package com.example.Surisuri_Masuri.question.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PatchUpdateQuestionReq {
+
+    private Integer questionIdx;
+    private String category;
+    private String title;
+    private String content;
+    private Boolean status;
+
+}
