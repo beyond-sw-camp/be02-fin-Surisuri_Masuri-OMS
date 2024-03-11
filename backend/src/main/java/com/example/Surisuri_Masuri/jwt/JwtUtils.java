dfcaf6288f0352f8639add817f86c12dcf76a8ff
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-11 20:07:48 +0900

    [Fix] JwtUtils 수정
      - Bearer split 메소드 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/jwt/JwtUtils.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/jwt/JwtUtils.java
@@ -75,4 +75,12 @@ public class JwtUtils {
                 .parseClaimsJws(token)
                 .getBody();
     }
+
+    public static String replaceToken(String token) {
+        if (token.startsWith("Bearer ")) {
+            token = token.split(" ")[1];
+        }
+
+        return token;
+    }
 }
