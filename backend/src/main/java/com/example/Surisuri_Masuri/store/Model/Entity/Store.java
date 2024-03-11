c4f14be8f51a9891a7e3cd2f36d030df523fad58
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-11 10:39:06 +0900

    [Feat] User & Store 간 연관 관계 매핑
      - User 회원 가입 기능 구현을 위한 최소한의 구현

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/store/Model/Entity/Store.java
@@ -0,0 +1,63 @@
+package com.example.Surisuri_Masuri.store.Model.Entity;
+
+import com.example.Surisuri_Masuri.member.Model.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idx;
+
+    private String storeName;
+
+    private String storeUuid;
+
+    private String storeAddr;
+
+    private String storePhoneNo;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userIdx")
+    private User user;
+
+    public void setStoreAddr(String storeAddr) {
+        this.storeAddr = storeAddr;
+    }
+
+    public void setStorePhoneNo(String storePhoneNo) {
+        this.storePhoneNo = storePhoneNo;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
