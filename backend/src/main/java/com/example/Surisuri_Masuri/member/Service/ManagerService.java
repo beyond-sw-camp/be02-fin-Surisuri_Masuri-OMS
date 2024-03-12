826231ead076b3cbc185e55fc0d22bcf4722eb06
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-12 11:08:26 +0900

    [Feat] Manager Login 및 SignUp 기능 구현
      - ManagerController 작성 및 수정
      - ManagerService 작성 및 수정
      - ManagerRepository 작성 및 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Service/ManagerService.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Service/ManagerService.java
@@ -3,7 +3,10 @@ package com.example.Surisuri_Masuri.member.Service;
 import com.example.Surisuri_Masuri.common.BaseResponse;
 import com.example.Surisuri_Masuri.jwt.JwtUtils;
 import com.example.Surisuri_Masuri.member.Model.Entity.Manager;
+import com.example.Surisuri_Masuri.member.Model.Entity.User;
+import com.example.Surisuri_Masuri.member.Model.ReqDtos.LoginReq;
 import com.example.Surisuri_Masuri.member.Model.ReqDtos.ManagerSignUpReq;
+import com.example.Surisuri_Masuri.member.Model.ResDtos.LoginRes;
 import com.example.Surisuri_Masuri.member.Model.ResDtos.ManagerSignUpRes;
 import com.example.Surisuri_Masuri.member.Repository.ManagerRepository;
 import lombok.RequiredArgsConstructor;
@@ -33,12 +36,12 @@ public class ManagerService {
     LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
     Date create = Date.from(localDateTime.atZone(ZoneId.of("Asia/Seoul")).toInstant());
     Date update = Date.from(localDateTime.atZone(ZoneId.of("Asia/Seoul")).toInstant());
-    ManagerSignUpRes managerSignUpRes;
 
+    // 회원가입 기능
     public BaseResponse<ManagerSignUpRes> ManagerSignUp(ManagerSignUpReq managerSignUpReq) {
 
             // 1. 이메일을 통해 이미 존재하는 회원인지 확인
-            if (managerRepository.findByManagerEmail(managerSignUpReq.getManagerEmail()).isPresent()) {
+            if (managerRepository.findByManagerId(managerSignUpReq.getManagerEmail()).isPresent()) {
                 return BaseResponse.failResponse(7000, "중복된 회원이 있습니다.");
             }
 
@@ -49,7 +52,7 @@ public class ManagerService {
                     .managerEmail(managerSignUpReq.getManagerEmail())
                     .managerName(managerSignUpReq.getManagerName())
                     .managerPhoneNo(managerSignUpReq.getManagerPhoneNo())
-                    .managerAuthority("manager")
+                    .managerAuthority("Manager")
                     .department("headquater")
                     .updatedAt(create)
                     .createdAt(update)
@@ -62,23 +65,40 @@ public class ManagerService {
             String accessToken = JwtUtils.generateAccessToken(manager, secretKey, expiredTimeMs);
 
             // 4. 응답 Dto 생성을 위한 과정
-            Optional<Manager> result = managerRepository.findByManagerEmail(manager.getManagerEmail());
+            Optional<Manager> result = managerRepository.findByManagerId(manager.getManagerEmail());
 
             if (result.isPresent())
             {
-                Manager manager2 = result.get();
-                managerSignUpRes = ManagerSignUpRes.builder()
-                        .token(accessToken)
-                        .managerName(manager2.getManagerName())
-                        .department(manager2.getDepartment())
-                        .managerId(manager2.getManagerId())
-                        .build();
+                manager = result.get();
             }
+        ManagerSignUpRes managerSignUpRes = ManagerSignUpRes.builder()
+                .token(accessToken)
+                .managerName(manager.getManagerName())
+                .department(manager.getDepartment())
+                .managerId(manager.getManagerId())
+                .build();
 
         return BaseResponse.successResponse("회원가입 완료", managerSignUpRes);
     }
-    public Manager getManagerByManagerEmail (String email){
-        Optional<Manager> manager = managerRepository.findByManagerEmail(email);
+
+    // 로그인 기능
+    public BaseResponse<LoginRes> ManagerLogin(LoginReq managerLoginReq) {
+        LoginRes loginRes = null;
+        Optional<Manager> manager = managerRepository.findByManagerId(managerLoginReq.getId());
+        if (manager.isEmpty()) {
+            return BaseResponse.failResponse(7000, "가입되지 않은 회원입니다.");
+        } else if (manager.isPresent() && passwordEncoder.matches(managerLoginReq.getPassword(), manager.get().getPassword()))
+            ;
+        {
+            loginRes = LoginRes.builder()
+                    .jwtToken(JwtUtils.generateAccessToken(manager.get(), secretKey, expiredTimeMs))
+                    .build();
+        }
+        return BaseResponse.successResponse("정상적으로 로그인 되었습니다.", loginRes);
+    }
+
+    public Manager getManagerByManagerId (String id){
+        Optional<Manager> manager = managerRepository.findByManagerId(id);
 
         if (manager.isPresent()) {
             return manager.get();
