d92c2990109fa2dc41202aa517e79be10ad761d3
DESKTOP-VFTL4HI\Playdata <zofnapf@naver.com>
2024-03-12 21:15:37 +0900

    [Chore] 실행을 위한 설정,메인 파일
    실행을 위한 설정, 메인 파일

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+import $ from 'jquery'
+import './assets/fonts.css';
+import './assets/global.css';
+window.$ = $
+window.jQuery = $
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router).mount('#app');
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// 빌드용
+// module.exports = {
+//     publicPath: "",
+//     // publicPath: "/dist/",
+//     // indexPath: "index.html",
+// };
