b2ceca2fd383aba88cd49a57464b42e6277481d1
jwj <llongkate@naver.com>
2024-03-13 19:13:16 +0900

    [Feat] 공지사항 페이지 개발
    공지사항 목록,작성 및 수정,삭제 페이지를 구현했습니다
    
    #66 #67 #68 #69

---
+++ frontend/manager/src/views/NoticeDetail.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container-fluid px-4">
+    <div class="container mt-5">
+      
+      <div class="mb-3">
+        <label class="form-label">제목</label>
+        <input type="text" class="form-control" v-model="notice.title" :readonly="!editable">
+      </div>
+      <div class="mb-3">
+        <label class="form-label">카테고리</label>
+        <input type="text" class="form-control" v-model="notice.category" readonly>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">작성일</label>
+        <input type="text" class="form-control" v-model="notice.date" readonly>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">내용</label>
+        <textarea class="form-control" v-model="notice.content" rows="5" :readonly="!editable"></textarea>
+      </div>
+      <div class="d-flex justify-content-between">
+        <router-link to="/notice" class="btn btn-secondary">목록으로</router-link>
+        <button v-if="!editable" @click="enableEditing" class="btn btn-primary">수정</button>
+        <button v-if="editable" @click="updateNotice" class="btn btn-success">저장</button>
+        <button @click="deleteNotice" class="btn btn-danger">삭제</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoticeDetail',
+  data() {
+    return {
+      notice: {
+        id: this.$route.params.id,
+        title: '',
+        category: '',
+        date: '',
+        content: '',
+      },
+      editable: false,
+    };
+  },
+  mounted() {
+    // TODO: 공지사항 ID에 해당하는 상세 정보를 불러오는 로직 구현
+    // 예시 데이터
+    this.notice = {
+      id: this.$route.params.id,
+      title: '공지사항 제목1',
+      category: '배송 관련',
+      date: '2024-03-15',
+      content: '공지사항 상세 내용입니다.',
+    };
+  },
+  methods: {
+    enableEditing() {
+      this.editable = true;
+    },
+    updateNotice() {
+      // TODO: 공지사항 수정 로직 구현
+      alert('공지사항이 업데이트되었습니다.');
+      this.editable = false;
+    },
+    deleteNotice() {
+      // TODO: 공지사항 삭제 로직 구현
+      alert('공지사항이 삭제되었습니다.');
+      this.$router.push('/notice');
+    },
+  },
+};
+</script>
+
+  
---
+++ frontend/manager/src/views/NoticeList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container-fluid px-4">
+       
+        <router-link to="/notice/new" class="btn btn-primary mb-3">공지사항 작성</router-link>
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        공지사항
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">카테고리</th>
+                                    <th scope="col">제목</th>
+                                    <th scope="col">내용</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(notice, index) in notices" :key="notice.id">
+                                    <th scope="row">{{ index + 1 }}</th>
+                                    <td>{{ notice.category }}</td>
+                                    <td>{{ notice.title }}</td>
+                                    <!-- <td>{{ notice.content }}</td> -->
+                                    <td>
+                                        <router-link :to="{ name: 'NoticeDetail', params: { id: notice.id } }" class="btn btn-primary btn-sm">상세 보기</router-link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NoticeList',
+    data() {
+      return {
+        notices: [
+          { id: 1, category: '배송 관련', title: '공지사항 제목1', content: ''},
+          { id: 2, category: '품질 관련', title: '공지사항 제목2', content: ''},
+          { id: 3, category: '입고 날짜 관련', title: '공지사항 제목3', content: ''},
+        ],
+      };
+    },
+  };
+  </script>
+  
