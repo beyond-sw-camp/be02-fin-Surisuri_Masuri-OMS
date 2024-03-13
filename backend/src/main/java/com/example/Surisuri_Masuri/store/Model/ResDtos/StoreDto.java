4d8edb3d9e12a24467d1ab622e336b49ba9a55ab
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-13 17:14:37 +0900

    [Feat] StoreDto 작성

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/store/Model/ResDtos/StoreDto.java
@@ -0,0 +1,13 @@
+package com.example.Surisuri_Masuri.store.Model.ResDtos;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StoreDto {
+
+    private String StoreAddr;
+    private String StoreName;
+
+}
