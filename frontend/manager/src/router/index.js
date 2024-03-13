9814a368e7cfcab023450e7f7a59f138b5bd40d5
jwj <llongkate@naver.com>
2024-03-13 17:29:45 +0900

    [Feat] 창고 페이지 개발
    창고 목록 조회 페이지 , 창고 재고 별 정보 페이지를 구현했습니다.
    
    #70 #71

--- frontend/manager/src/router/index.js
+++ frontend/manager/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
+import ContainerList from '../views/ContainerList.vue';
+import ContainerDetail2 from '@/views/ContainerDetail2.vue';
 
 
 const routes = [
@@ -15,6 +17,16 @@ const routes = [
     name: 'login',
     component: LoginView // 로그인 컴포넌트
   },
+  {
+    path: '/container',
+    name: 'ContainerList',
+    component: ContainerList,
+},
+  {
+    path: '/containerdetail',
+    name: 'ContainerDetail2',
+    component: ContainerDetail2,
+},
   
 
   // 여기에 다른 라우트를 추가할 수 있습니다.
