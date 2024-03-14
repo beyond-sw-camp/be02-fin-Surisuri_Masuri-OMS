9348c5625572147f906992315b01cab4b7cf83d5
DESKTOP-VFTL4HI\Playdata <zofnapf@naver.com>
2024-03-14 10:42:05 +0900

    [Feat] Component router 설정
    router 설정
    
    #87

--- frontend/manager/src/router/index.js
+++ frontend/manager/src/router/index.js
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue';
-
+import StoreView from '../views/StoreView.vue';
+import StoreDetailView from '../views/StoreDetailView';
+import AdminInquiryList from '../views/AdminInquiryList.vue';
+import AdminInquiryDetail from '../views/AdminInquiryDetail.vue';
 
 const routes = [
   {
@@ -15,8 +18,28 @@ const routes = [
     name: 'login',
     component: LoginView // 로그인 컴포넌트
   },
-  
-
+  {
+    path: '/store', 
+    name: 'storeview',
+    component: StoreView // 로그인 컴포넌트
+  },
+  {
+    path: '/order-detail/:orderNumber',
+    name: 'StoreDetail',
+    component: StoreDetailView,
+    props: true // 주문 상세 정보를 표시할 컴포넌트
+  },
+  {
+    path: '/inquiries',
+    name: 'AdminInquiryList',
+    component: AdminInquiryList,
+  },
+  {
+    path: '/inquiries/:id',
+    name: 'AdminInquiryDetail',
+    component: AdminInquiryDetail,
+    props: true, // props를 통해 파라미터를 컴포넌트에 전달할 수 있게 합니다.
+  },
   // 여기에 다른 라우트를 추가할 수 있습니다.
 ]
 
