2f8e02ea0c361ee226e5aac2b338b610c5cc10b1
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-14 18:26:42 +0900

    [Fix] 가맹점 재고 요청 DTO 수정
    유통기간 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Model/ReqDtos/StoreStockCreateReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Model/ReqDtos/StoreStockCreateReq.java
@@ -3,11 +3,14 @@ package com.example.Surisuri_Masuri.storeStock.Model.ReqDtos;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class StoreStockCreateReq {
 
     private Long productIdx;
     private Long stockQuantitiy;
+    private LocalDate expiredAt;
 
 }
