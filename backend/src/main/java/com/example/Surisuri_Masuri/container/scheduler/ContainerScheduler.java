c1d965fde7bbc5046aa737997d344fb5a29d3cd5
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-15 18:29:02 +0900

    [Feat] Container Batch 적용
      - ContainerScheduler 작성
      - ContainerSchedulingService 작성
    
    #82

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/container/scheduler/ContainerScheduler.java
@@ -0,0 +1,20 @@
+package com.example.Surisuri_Masuri.container.scheduler;
+
+import com.example.Surisuri_Masuri.container.service.ContainerSchedulingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class ContainerScheduler {
+    private final ContainerSchedulingService containerSchedulingService;
+
+    @Scheduled(cron = "*/30 * 17 * * *\n") // 1초에 1000
+    public void test() throws IOException {
+        System.out.println("test");
+        containerSchedulingService.discardExpiredFoodProducts();
+    }
+}
