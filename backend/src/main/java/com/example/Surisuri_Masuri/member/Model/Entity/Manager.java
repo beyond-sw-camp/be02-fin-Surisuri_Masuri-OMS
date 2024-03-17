3a183354ab7eee4746ffb88dd21f50011b3de453
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-17 22:30:37 +0900

    [Feat] Entity 유효성 검증 설정
      - Manager Entity

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/Entity/Manager.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/Entity/Manager.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import java.util.*;
 
 @Entity
@@ -16,26 +18,44 @@ import java.util.*;
 @Builder
 public class Manager implements UserDetails {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "영어와 숫자만 가능합니다.")
     private String managerId;
 
-    private String managerPassword;
-
-    private String managerEmail;
-
+    @Column(nullable = false)
+    @Pattern(regexp = "^[가-힣]*$", message = "한글만 가능합니다.")
     private String managerName;
 
+    @Column(nullable = false)
+    @Pattern(regexp = "^(\\d{2,3}-?\\d{3,4}-?\\d{4}|\\d{10,11})$", message = "올바른 전화번호 형식이어야 합니다.")
     private String managerPhoneNo;
 
-    private String managerAuthority;
-
+    @Column(nullable = false)
+    @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "영어와 한글만 가능합니다.")
     private String department;
 
+    @Column(nullable = false, unique = true)
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    private String managerEmail;
+
+    @Column(nullable = false)
+    @Pattern(regexp = "^(?=.*[A-Z])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]{8,15}$", message = "비밀번호는 대문자를 포함한 8~15자의 영문, 숫자, 특수문자(!@#$%^&*()_+-=[])만 가능합니다.")
+    private String managerPassword;
+
+    @Column(nullable = false)
+    private String managerAuthority;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
     private Date updatedAt;
 
     @OneToMany(mappedBy = "manager")
