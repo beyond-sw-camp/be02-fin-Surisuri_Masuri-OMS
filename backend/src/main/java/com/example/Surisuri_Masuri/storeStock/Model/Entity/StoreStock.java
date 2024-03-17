6ff465b42176e982663dbf71cf48ff09cc79c9f8
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-17 22:31:22 +0900

    [Feat] Entity 유효성 검증 설정
      - StoreStock Entity

--- backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Model/Entity/StoreStock.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Model/Entity/StoreStock.java
@@ -4,6 +4,7 @@ import com.example.Surisuri_Masuri.product.model.Product;
 import com.example.Surisuri_Masuri.store.Model.Entity.Store;
 import lombok.*;
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -19,13 +20,19 @@ public class StoreStock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    @Column(nullable = false)
     private Long stockQuantitiy;
 
+    @Column(nullable = false)
     private LocalDate expiredAt;
 
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
+
+    @Column(nullable = false)
     private Date updatedAt;
 
+    @Column(nullable = false)
     private Boolean isDiscarded; // 폐기 여부
 
     private LocalDate discardedAt; // 폐기 일자
