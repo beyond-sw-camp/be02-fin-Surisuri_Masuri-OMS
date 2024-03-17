8eccea1167eae42d064b4c6e5540bbfde1ca7185
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-17 22:31:27 +0900

    [Feat] Entity 유효성 검증 설정
      - User Entity

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/Entity/User.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/Entity/User.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import java.util.*;
 
 @Entity
@@ -21,14 +23,23 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    @Column(nullable = false, unique = true)
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
     private String userEmail;
 
+    @Column(nullable = false)
+    @Pattern(regexp = "^(?=.*[A-Z])[A-Za-z\\d!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]{8,15}$", message = "비밀번호는 대문자를 포함한 8~15자의 영문, 숫자, 특수문자(!@#$%^&*()_+-=[])만 가능합니다.")
     private String userPassword;
 
+    @Column(nullable = false)
+    @Pattern(regexp = "^[가-힣]*$", message = "한글만 가능합니다.")
     private String userName;
 
+    @Column(nullable = false)
+    @Pattern(regexp = "^(\\d{2,3}-?\\d{3,4}-?\\d{4}|\\d{10,11})$", message = "올바른 전화번호 형식이어야 합니다.")
     private String userPhoneNo;
 
+    @Column(nullable = false, length = 1)
     private Boolean status;
 
     public void setUserPassword(String userPassword) {
@@ -39,10 +50,13 @@ public class User implements UserDetails {
         this.userPhoneNo = userPhoneNo;
     }
 
+    @Column(nullable = false)
     private String userAuthority;
 
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
 
+    @Column(nullable = false)
     private Date updatedAt;
 
     @OneToMany(mappedBy = "user")
