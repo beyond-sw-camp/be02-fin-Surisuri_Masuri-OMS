422c71895af400dedda3371d9ea2f36f5e71c730
DESKTOP-VFTL4HI\Playdata <zofnapf@naver.com>
2024-03-17 19:17:50 +0900

    [Feat]Notice axios 연결
    가맹점 공지사항 조회
    
    (#56)

--- frontend/store/src/views/NofiDetailView.vue
+++ frontend/store/src/views/NofiDetailView.vue
@@ -8,9 +8,8 @@
                         공지사항 상세
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">{{ notice.title }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ notice.date }}</h6>
-                        <p class="card-text">{{ notice.content }}</p>
+                        <p>제목: {{ this.$route.query.title }}</p>
+                        <p>내용: {{ this.$route.query.content }}</p>
                         <a @click="goBack" class="card-link">목록으로 돌아가기</a>
                     </div>
                 </div>
@@ -19,23 +18,13 @@
     
     </div>
 </template>
-
 <script>
 export default {
-    data() {
-        return {
-            notice: {},
-        };
+  methods: {
+    goBack() {
+      this.$router.push('/nofi'); // 공지사항 목록 페이지로 이동
     },
-    mounted() {
-        const id = this.$route.params.id;
-        // 여기서 id에 해당하는 공지사항 데이터를 불러와서 notice 객체를 업데이트합니다.
-        this.notice = { id: id, title: '공지사항 A', date: '2024-03-10', content: '공지사항 A의 내용입니다.' };
-    },
-    methods: {
-        goBack() {
-            this.$router.go(-1);
-        }
-    }
+  },
 };
 </script>
+
--- frontend/store/src/views/NofiView.vue
+++ frontend/store/src/views/NofiView.vue
@@ -13,18 +13,25 @@
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">제목</th>
-                                    <th scope="col">작성일</th>
+                                    <th scope="col">내용</th>
                                     <th scope="col">상세보기</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr v-for="(notice, index) in notices" :key="notice.id">
+                                <tr v-for="(notice, index) in notices" :key="notice.idx">
                                     <th scope="row">{{ index + 1 }}</th>
                                     <td>{{ notice.title }}</td>
-                                    <td>{{ notice.date }}</td>
+                                    <td>{{ notice.content }}</td>
                                     <td>
-                                        <button @click="goToNofiDetail(notice.id)" class="btn btn-primary btn-sm">상세보기</button>
-                                    </td>
+                                  </td>    
+                                  <router-link :to="{
+                                    name: 'NoticeDetail',
+                                    query: {
+                                        idx: notice.idx,
+                                        title: notice.title,
+                                        content: notice.content
+                                    }
+                                }" class="btn btn-primary btn-sm">상세 보기</router-link>
                                 </tr>
                             </tbody>
                         </table>
@@ -36,21 +43,39 @@
 
 </template>
 
+
 <script>
+import axios from 'axios';
+
 export default {
-    data() {
-        return {
-            notices: [
-                { id: 1, title: '공지사항 A', date: '2024-03-10' },
-                { id: 2, title: '공지사항 B', date: '2024-03-09' },
-                // 추가 공지사항 데이터...
-            ],
-        };
+  data() {
+    return {
+      notices: [],
+    };
+  },
+  created() {
+    this.fetchNotices();
+  },
+  methods: {
+    async fetchNotices() {
+      try {
+        const response = await axios.get('http://localhost:8080/notice/list', {
+          params: {
+            page: 1,
+            size: 1, // size 값을 조정했습니다. 예시로 1이 아니라 10으로 설정
+          }
+        });
+        // 응답 데이터 구조에 맞게 notices 배열에 데이터 저장
+        this.notices = response.data.result; // 수정: 'data.data'에서 'data.result'로 변경
+      } catch (error) {
+        console.error('공지사항 목록을 불러오는 중 오류가 발생했습니다.', error);
+      }
     },
-    methods: {
-        goToNofiDetail(id) {
-            this.$router.push({ name: 'NofiDetailView', params: { id: id }});
-        },
+    goToNofiDetail(idx) {
+      this.$router.push({ name: 'NofiDetailView', params: { idx: idx }});
     },
-}; 
+  },
+};
 </script>
+
+  
