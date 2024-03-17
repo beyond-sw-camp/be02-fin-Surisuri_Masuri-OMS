f96756038ba55fe0df32ddeb3e1544e851d123f1
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-17 19:37:34 +0900

    [Feat] Exception 설정
      - ErrorCode 작성
      - ErrorResponse 작성
      - GlobalExceptionHandlerExceptionHandler 작성
      - BusinessException 작성

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.example.Surisuri_Masuri.exception;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.nio.file.AccessDeniedException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // 일관된 예외처리를 하도록 BusinessException을 상속
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    // 401 에러 처리 - 토큰
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidTokenException(AccessDeniedException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_TOKEN;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    // 401 에러 처리 - 권한
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidPermissionException(AccessDeniedException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_PERMISIION;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    // 404 에러 처리
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        ErrorCode errorCode = ErrorCode.CONSTRAINT_VIOLATION;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    // 500 에러 처리
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+}
