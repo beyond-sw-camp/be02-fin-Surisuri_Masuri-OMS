0cab493cf11846d58263cf142a1c532ddf58a912
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-18 12:10:34 +0900

    [Feat] ContainerSchedulingService 예외처리 구현

--- backend/src/main/java/com/example/Surisuri_Masuri/container/service/ContainerSchedulingService.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/container/service/ContainerSchedulingService.java
@@ -5,6 +5,8 @@ import com.example.Surisuri_Masuri.container.model.entity.ContainerStock;
 import com.example.Surisuri_Masuri.container.model.request.ContainerStockDto;
 import com.example.Surisuri_Masuri.container.repository.ContainerRepository;
 import com.example.Surisuri_Masuri.container.repository.ContainerStockRepository;
+import com.example.Surisuri_Masuri.exception.EntityException.ContainerStockException;
+import com.example.Surisuri_Masuri.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,10 +21,17 @@ public class ContainerSchedulingService {
     private final ContainerStockRepository containerStockRepository;
 
     public BaseResponse<List<ContainerStockDto>> discardExpiredFoodProducts() {
+
         // 현재 날짜를 기준으로 1주일 전의 날짜를 계산합니다.
         LocalDate currentDate = LocalDate.now().plusDays(7);
+
         // 유통기한이 1주일 남은 식품 상품들을 조회합니다.
         List<ContainerStock> expiredFoodProducts = containerStockRepository.findExpiredFoodProducts(currentDate);
+
+        if(expiredFoodProducts.isEmpty())
+        {
+            throw new ContainerStockException(ErrorCode.ContainerStock_002,("No Product for Discard."));
+        }
 
         // 조회된 상품들을 폐기 처리합니다.
         for (ContainerStock containerStock : expiredFoodProducts) {
