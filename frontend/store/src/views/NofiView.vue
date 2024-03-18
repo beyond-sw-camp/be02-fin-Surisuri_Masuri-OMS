53e8261ce01b5d2979c615f6016e7544651f27a7
DESKTOP-VFTL4HI\Playdata <zofnapf@naver.com>
2024-03-18 14:56:01 +0900

    [Design] 공지사항 디자인
    디자인 수정
    
    (#56)

--- frontend/store/src/views/NofiView.vue
+++ frontend/store/src/views/NofiView.vue
@@ -1,48 +1,51 @@
 <template>
-    <div class="container-fluid px-4">
-        
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        공지사항
-                    </div>
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">제목</th>
-                                    <th scope="col">내용</th>
-                                    <th scope="col">상세보기</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="(notice, index) in notices" :key="notice.idx">
-                                    <th scope="row">{{ index + 1 }}</th>
-                                    <td>{{ notice.title }}</td>
-                                    <td>{{ notice.content }}</td>
-                                    <td>
-                                  </td>    
-                                  <router-link :to="{
-                                    name: 'NoticeDetail',
-                                    query: {
-                                        idx: notice.idx,
-                                        title: notice.title,
-                                        content: notice.content
-                                    }
-                                }" class="btn btn-primary btn-sm">상세 보기</router-link>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+  <div class="container-fluid px-4">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-header">
+            공지사항
+          </div>
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">제목</th>
+                  <th scope="col">내용</th>
+                  <th scope="col">상세보기</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(notice, index) in notices" :key="notice.idx">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ notice.title }}</td>
+                  <td>{{ notice.content }}</td>
+                  <td>
+                    <router-link
+                      :to="{
+                        name: 'NoticeDetail',
+                        query: {
+                          idx: notice.idx,
+                          title: notice.title,
+                          content: notice.content,
+                          category: notice.category
+                        }
+                      }"
+                      class="btn btn-primary btn-sm"
+                    >
+                      상세 보기
+                    </router-link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     </div>
-
+  </div>
 </template>
-
 
 <script>
 import axios from 'axios';
@@ -62,20 +65,14 @@ export default {
         const response = await axios.get('http://localhost:8080/notice/list', {
           params: {
             page: 1,
-            size: 1, // size 값을 조정했습니다. 예시로 1이 아니라 10으로 설정
+            size: 10,
           }
         });
-        // 응답 데이터 구조에 맞게 notices 배열에 데이터 저장
-        this.notices = response.data.result; // 수정: 'data.data'에서 'data.result'로 변경
+        this.notices = response.data.result;
       } catch (error) {
         console.error('공지사항 목록을 불러오는 중 오류가 발생했습니다.', error);
       }
     },
-    goToNofiDetail(idx) {
-      this.$router.push({ name: 'NofiDetailView', params: { idx: idx }});
-    },
   },
 };
 </script>
-
-  
