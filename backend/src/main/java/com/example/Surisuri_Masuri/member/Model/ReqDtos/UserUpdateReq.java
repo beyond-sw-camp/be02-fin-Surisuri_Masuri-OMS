99b71324bcf6229defb168f534a1a3020ec7d19d
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-18 10:18:43 +0900

    [Fix] UserUpdateReq 코드 수정
      - 유효값 검증 관련 코드 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserUpdateReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserUpdateReq.java
@@ -3,11 +3,19 @@ package com.example.Surisuri_Masuri.member.Model.ReqDtos;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder
 public class UserUpdateReq {
     private String storeAddr;
+
+    @Pattern(regexp = "^(\\d{2,3}-?\\d{3,4}-?\\d{4}|\\d{10,11})$", message = "올바른 전화번호 형식이어야 합니다.")
     private String storePhoneNo;
+
+    @Pattern(regexp = "^(\\d{2,3}-?\\d{3,4}-?\\d{4}|\\d{10,11})$", message = "올바른 전화번호 형식이어야 합니다.")
     private String userPhoneNo;
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$")
     private String userPassword;
 }
