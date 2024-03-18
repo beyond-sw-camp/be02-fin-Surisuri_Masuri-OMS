1385d85d9baa44cd8d830a65e325230c5f7c42e1
Hyeon-Kyun <149039986+Hyeon-Kyun@users.noreply.github.com>
2024-03-18 22:49:39 +0900

    Revert "Frontend/feature/managerContainer"

--- backend/src/main/java/com/example/Surisuri_Masuri/container/repository/ContainerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Surisuri_Masuri.container.repository;
-
-import com.example.Surisuri_Masuri.container.model.entity.Container;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContainerRepository extends JpaRepository<Container, Integer> {
-
-
-}
--- backend/src/main/java/com/example/Surisuri_Masuri/store/Repository/QueryDsl/StoreRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.Surisuri_Masuri.store.Repository.QueryDsl;
-
-import com.example.Surisuri_Masuri.store.Model.Entity.Store;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface StoreRepositoryCustom {
-    public Page<Store> findList(Pageable pageable);
-}
