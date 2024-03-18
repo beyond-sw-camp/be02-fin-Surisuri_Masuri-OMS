fe969779c882e4c178ba16a9e5fe172f5a751853
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-18 10:20:48 +0900

    [Feat] GlobalExceptionHandler 코드 수정
      - Valid 관련 메소드 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.example.Surisuri_Masuri.exception;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +12,29 @@ import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // @Valid를 통한 입력값 검증 시 발생하는 에러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidException(MethodArgumentNotValidException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (int i=0; i<bindingResult.getFieldErrors().size(); i++) {
+            FieldError fieldError = bindingResult.getFieldErrors().get(i);
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+
+            if(i<bindingResult.getFieldErrors().size()-1) {
+                builder.append(" / ");
+            }
+        }
+
+        ErrorResponse response = new ErrorResponse(errorCode.getCode(), builder.toString());
+        return new ResponseEntity<>(response, errorCode.getStatus());
+    }
 
     // 일관된 예외처리를 하도록 BusinessException을 상속
     @ExceptionHandler(BusinessException.class)
@@ -21,7 +47,7 @@ public class GlobalExceptionHandler {
     // 401 에러 처리 - 권한
     @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<ErrorResponse> handleInvalidPermissionException(AccessDeniedException e) {
-        ErrorCode errorCode = ErrorCode.INVALID_PERMISIION;
+        ErrorCode errorCode = ErrorCode.INVALID_PERMISION;
         ErrorResponse response = new ErrorResponse(errorCode.getCode(), e.getMessage());
         return new ResponseEntity<>(response, errorCode.getStatus());
     }
