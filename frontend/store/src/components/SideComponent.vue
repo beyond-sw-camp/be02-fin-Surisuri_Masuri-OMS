42b5441ed594bc1037f1f0dbab468cf8a20205cd
jwj <llongkate@naver.com>
2024-03-19 11:20:33 +0900

    [Fix] 가맹점 관리자 공통 컴포넌트 수정
    가맹점 관리자의 공통 컴포넌트 중 목차의 구성을 변경했습니다.
    
    #72

--- frontend/store/src/components/SideComponent.vue
+++ frontend/store/src/components/SideComponent.vue
@@ -1,131 +1,121 @@
 <template>
-    <aside class="left-sidebar">
-      <!-- Sidebar scroll-->
-      <div>
-        <div class="brand-logo d-flex align-items-center justify-content-between">
-          <router-link to="/home" class="text-nowrap logo-img">
-            <img src="../assets/Group24.png" width="100%" alt="">
-          </router-link>
-          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-           
-          </div>
-        </div>
-        <!-- Sidebar navigation-->
-        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
-          <ul id="sidebarnav">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">Home</span>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/home">
-                <span>
-                  <i class="ti ti-layout-dashboard"></i>
-                </span>
-                <span class="hide-menu">Dashboard</span>
-              </router-link>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
-              <span class="hide-menu">가맹점 관리</span>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/product">
-                <span>
-                  <i class="ti ti-article"></i>
-                </span>
-                <span class="hide-menu">상품발주</span>
-              </router-link>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/orderdetail">
-                <span>
-                  <i class="ti ti-wallet"></i>
-                </span>
-                <span class="hide-menu">주문내역</span>
-              </router-link>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/stockedit">
-                <span>
-                  <i class="ti ti-alert-circle"></i>
-                </span>
-                <span class="hide-menu">재고관리</span>
-              </router-link>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/userprofile">
-                <span>
-                  <i class="ti ti-cards"></i>
-                </span>
-                <span class="hide-menu">마이페이지</span>
-              </router-link>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/nofi">
-                <span>
-                  <i class="ti ti-file-description"></i>
-                </span>
-                <span class="hide-menu">공지사항</span>
-              </router-link>
-            </li>
-            <li class="sidebar-item">
-              <router-link class="sidebar-link" to="/inquiry">
-                <span>
-                  <i class="ti ti-typography"></i>
-                </span>
-                <span class="hide-menu">문의사항</span>
-              </router-link>              
-            </li>
-          </ul>
-        </nav>
-        <!-- End Sidebar navigation -->
+  <aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div>
+      <div class="brand-logo d-flex align-items-center justify-content-between">
+        <router-link to="/home" class="text-nowrap logo-img">
+          <img src="../assets/Group24.png" width="100%" alt="" />
+        </router-link>
+        <div
+          class="close-btn d-xl-none d-block sidebartoggler cursor-pointer"
+          id="sidebarCollapse"
+        ></div>
       </div>
-      
-      
-    </aside>
-    
-  </template>
+      <!-- Sidebar navigation-->
+      <nav
+        data-v-2cf85266=""
+        class="sidebar-nav scroll-sidebar"
+        data-simplebar=""
+      >
+        <ul data-v-2cf85266="" id="sidebarnav">
+          <li data-v-2cf85266="" class="nav-small-cap">
+            <i data-v-2cf85266="" class="ti ti-dots nav-small-cap-icon fs-4"></i
+            ><span data-v-2cf85266="" class="hide-menu"></span>
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a
+              data-v-2cf85266=""
+              href="/home"
+              class="router-link-active router-link-exact-active sidebar-link"
+              aria-current="page">
+              <span data-v-2cf85266="">
+              <i data-v-2cf85266="" class="ti ti-layout-dashboard"></i>
+              </span>
+              <span data-v-2cf85266="" class="hide-menu">HOME</span>
+            </a>
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/product" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-article"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">상품발주</span></a
+            >
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/orderdetail" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-wallet"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">주문내역</span></a
+            >
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/stockedit" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-alert-circle"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">재고관리</span></a
+            >
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/userprofile" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-cards"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">마이페이지</span></a
+            >
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/nofi" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-file-description"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">공지사항</span></a
+            >
+          </li>
+          <li data-v-2cf85266="" class="sidebar-item">
+            <a data-v-2cf85266="" href="/inquiry" class="sidebar-link"
+              ><span data-v-2cf85266=""
+                ><i data-v-2cf85266="" class="ti ti-typography"></i></span
+              ><span data-v-2cf85266="" class="hide-menu">문의사항</span></a
+            >
+          </li>
+        </ul>
+      </nav>
+      <!-- End Sidebar navigation -->
+    </div>
+  </aside>
+</template>
 
-<script>
-
-</script>
+<script></script>
 
 <style scoped>
-
 .left-sidebar {
-    height: 100vh; 
-    min-width: 18%; 
-    z-index: 1000; 
-    position: relative; 
+  height: 100vh;
+  min-width: 18%;
+  z-index: 1000;
+  position: relative;
 }
 
 .left-sidebar a {
-    text-decoration: none; /* 링크의 밑줄 제거 */
-    color: inherit;
-     /* 링크 색상을 상위 요소의 색상과 동일하게 설정 */
+  text-decoration: none; /* 링크의 밑줄 제거 */
+  color: inherit;
+  /* 링크 색상을 상위 요소의 색상과 동일하게 설정 */
 }
 
-
 .sidebar-item .hide-menu {
-    font-size: 19px; /* 세부 목차 글씨 크기 조정 */
+  font-size: 19px; /* 세부 목차 글씨 크기 조정 */
 }
 
 .nav-small-cap {
-    font-size: 18px;
-    margin-top: 30px;
-    margin-bottom: 30px;
+  font-size: 18px;
+  margin-top: 30px;
+  margin-bottom: 5px;
 }
 
 /* 세부 항목 글씨 크기 및 간격 조정 */
 .sidebar-item {
-    margin-bottom: 20px; /* 항목 사이의 간격 늘리기 */
+  margin-bottom: 20px; /* 항목 사이의 간격 늘리기 */
 }
 
 .giga-coffee {
-    width: 80px;
-    height: 150px;
+  width: 80px;
+  height: 150px;
 }
-
 </style>
-
