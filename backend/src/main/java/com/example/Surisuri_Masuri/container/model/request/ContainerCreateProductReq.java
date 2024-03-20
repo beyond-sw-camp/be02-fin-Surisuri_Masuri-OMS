32126ecbc38e29869b4c0b41604624fe211df7f0
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-20 09:33:55 +0900

    [Fix] ContainerCreateProductReq 수정
      - Lombok 어노테이션 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/container/model/request/ContainerCreateProductReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/container/model/request/ContainerCreateProductReq.java
@@ -1,12 +1,14 @@
 package com.example.Surisuri_Masuri.container.model.request;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Getter
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContainerCreateProductReq {
     Integer containerIdx;
     Long productIdx;
