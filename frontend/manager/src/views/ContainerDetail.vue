bf267d474bb0965da7dc2c68b6ebd30655e60ccf
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-25 02:45:39 +0900

    [Feat] 창고 개별 조회 페이지 수정 (#71)
    창고 목록 조회에서 idx 받아와 창고 개별 조회하는 axios 추가

--- frontend/manager/src/views/ContainerDetail.vue
+++ frontend/manager/src/views/ContainerDetail.vue
@@ -1,54 +1,61 @@
 <template>
-    <div class="container-fluid px-4">
-        
-        <div class="row">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">
-                        상품 목록
-                    </div>
-                    <div class="card-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">상품 이름</th>
-                                    <th scope="col">재고</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr v-for="(containers, index) in containerss" :key="containers.id">
-                                    <th scope="row">{{ index + 1 }}</th>
-                                    <td>{{ containers.productName }}</td>
-                                    <td>{{ containers.productQuantity }}</td>
-                                    <td>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <a @click="goBack" class="btn btn-primary btn-sm">목록으로 돌아가기</a>
-                    </div>
-                </div>
-            </div>
+  <div class="container-fluid px-4">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-header">상품 목록</div>
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">상품 이름</th>
+                  <th scope="col">재고</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(container, index) in containerss" :key="container.containerIdx">
+                  <th scope="row">{{ index + 1 }}</th>
+                  <td>{{ container.productName }}</td>
+                  <td>{{ container.productQuantity }}</td>
+                </tr>
+              </tbody>
+            </table>
+            <a @click="goBack" class="btn btn-primary btn-sm">목록으로 돌아가기</a>
+          </div>
         </div>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    data() {
-        return {
-            containerss: [
-                { id: 1, productName: '상품1', productQuantity: '100' },
-                { id: 2, productName: '상품2', productQuantity: '200' },
-            ],
-        };
+  data() {
+    return {
+      containerss: [],
+    };
+  },
+  methods: {
+    async fetchData(containerIdx) {
+      try {
+        // 두 번째 템플릿으로 전달된 컨테이너의 idx 값을 사용하여 데이터를 받아옴
+        const response = await axios.get(`http://localhost:8080/container/singlestock?containerIdx=${containerIdx}`);
+        this.containerss = response.data.result; // 받아온 데이터를 containerss에 저장
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     },
-    methods: {
-        goBack() {
-            this.$router.go(-1);
-        }
-    }
-}; 
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+  mounted() {
+    // 두 번째 템플릿이 마운트되면서 컨테이너의 idx 값을 받아와서 데이터를 불러옴
+    const containerIdx = this.$route.params.containerIdx;
+    this.fetchData(containerIdx);
+  },
+};
 </script>
