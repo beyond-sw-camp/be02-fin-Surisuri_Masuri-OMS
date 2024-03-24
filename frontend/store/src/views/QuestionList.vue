fc98ba9a92125231542e96ea3fdeec1c64146f32
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-25 03:30:51 +0900

    [Fix] 가맹점 문의사항 조회 페이지 수정 (#55)
    가맹점 문의사항 조회 페이지에서 상세 페이지로 갈 때 idx값 보내도록 수정

--- frontend/store/src/views/QuestionList.vue
+++ frontend/store/src/views/QuestionList.vue
@@ -27,16 +27,17 @@
                       :to="{
                         name: 'QuestionDetail',
                         query: {
-                          idx: question.idx,
+                          idx: question.questionIdx,
                           title: question.title,
                           content: question.content,
                           category: question.category,
                           status: question.status,
-                          answerContent: question.answerContent
-                        }
+                          answerContent: question.answerContent,
+                        },
                       }"
                       class="btn btn-primary btn-sm"
-                    >상세 보기</router-link>
+                      >상세 보기</router-link
+                    >
                   </td>
                 </tr>
               </tbody>
@@ -48,50 +49,41 @@
   </div>
 </template>
 
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    data() {
-      return {
-        questions: [], // 문의사항 목록을 저장할 배열
-        pagination: {
-          // 페이지네이션 정보를 저장할 객체
-          page: 1, // 기본 페이지 번호
-          size: 10, // 기본 페이지당 항목 수, 예시로 10으로 설정
-        },
-      };
-    },
-    created() {
-      this.fetchQuestions(); // 컴포넌트 생성 시 문의사항 목록을 불러오도록 설정
-    },
-    methods: {
-      async fetchQuestions() {
-    try {
-      const response = await axios.get(
-        "http://192.168.0.162/question/list",
-        {
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      questions: [], // 문의사항 목록을 저장할 배열
+      pagination: {
+        // 페이지네이션 정보를 저장할 객체
+        page: 1, // 기본 페이지 번호
+        size: 10, // 기본 페이지당 항목 수, 예시로 10으로 설정
+      },
+    };
+  },
+  created() {
+    this.fetchQuestions(); // 컴포넌트 생성 시 문의사항 목록을 불러오도록 설정
+  },
+  methods: {
+    async fetchQuestions() {
+      try {
+        const response = await axios.get("http://localhost:8080/question/list", {
           headers: {
             Authorization: "Bearer " + sessionStorage.getItem("token"),
           },
           params: this.pagination, // 페이지네이션 정보를 params로 전달
-        }
-      );
-      // 응답 데이터 전체를 콘솔에 출력
-      console.log("응답 데이터:", response.data);
-  
-      // 응답 데이터 구조에 맞게 questions 배열에 데이터 저장
-      this.questions = response.data.result; // 수정: 응답에서 받은 데이터를 questions 배열에 저장
-    } catch (error) {
-      console.error(
-        "문의사항 목록을 불러오는 중 오류가 발생했습니다.",
-        error
-      );
-    }
-  },
-  
+        });
+        // 응답 데이터 전체를 콘솔에 출력
+        console.log("응답 데이터:", response.data);
+
+        // 응답 데이터 구조에 맞게 questions 배열에 데이터 저장
+        this.questions = response.data.result; // 수정: 응답에서 받은 데이터를 questions 배열에 저장
+      } catch (error) {
+        console.error("문의사항 목록을 불러오는 중 오류가 발생했습니다.", error);
+      }
     },
-  };
-  </script>
-  
+  },
+};
+</script>
