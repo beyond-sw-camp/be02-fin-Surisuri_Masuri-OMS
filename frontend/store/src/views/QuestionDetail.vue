6d6694e1d7f0455ff20b0dd0aab1b08084e2460f
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-25 03:29:22 +0900

    [Fix] 가맹점 문의사항 상세 페이지 수정 (#55)
    idx값 받아올 수 있도록 수정

--- frontend/store/src/views/QuestionDetail.vue
+++ frontend/store/src/views/QuestionDetail.vue
@@ -2,41 +2,25 @@
   <div class="container mt-5">
     <h2 class="mb-3">문의 상세 정보</h2>
     <div class="mb-3">
+      <label class="form-label">문의 번호:</label>
+      <input type="text" class="form-control" v-model="questionIdx" :readonly="!editable" />
+    </div>
+    <div class="mb-3">
       <label class="form-label">문의 제목:</label>
-      <input
-        type="text"
-        class="form-control"
-        v-model="questionTitle"
-        :readonly="!editable"
-      />
+      <input type="text" class="form-control" v-model="questionTitle" :readonly="!editable" />
     </div>
     <div class="mb-3">
       <label class="form-label">문의 내용:</label>
-      <textarea
-        class="form-control"
-        v-model="questionContent"
-        rows="3"
-        :readonly="!editable"
-      ></textarea>
+      <textarea class="form-control" v-model="questionContent" rows="3" :readonly="!editable"></textarea>
     </div>
     <div class="mb-3">
       <label class="form-label">카테고리:</label>
-      <input
-        type="text"
-        class="form-control"
-        v-model="questionCategory"
-        :readonly="!editable"
-      />
+      <input type="text" class="form-control" v-model="questionCategory" :readonly="!editable" />
     </div>
     <!-- answerContent가 있을 때만 보이는 섹션 -->
     <div class="mb-3" v-if="answerContent">
       <label class="form-label">답변 내용:</label>
-      <textarea
-        class="form-control"
-        v-model="answerContent"
-        rows="3"
-        readonly
-      ></textarea>
+      <textarea class="form-control" v-model="answerContent" rows="3" readonly></textarea>
     </div>
     <div class="d-flex justify-content-between">
       <router-link to="/question" class="btn btn-secondary">목록으로 돌아가기</router-link>
@@ -74,7 +58,7 @@ export default {
       }
       try {
         await axios.patch(
-          "http://192.168.0.162/question/update",
+          "http://localhost:8080/question/update",
           {
             idx: this.questionIdx,
             title: this.questionTitle,
@@ -92,13 +76,11 @@ export default {
         );
         alert("변경 사항이 저장되었습니다.");
         this.editable = false;
-        this.$router.push('/question'); // 수정 후 inquiry 목록으로 리다이렉트
+        this.$router.push("/question"); // 수정 후 inquiry 목록으로 리다이렉트
       } catch (error) {
         alert(
           "문의사항 수정 중 오류가 발생했습니다: " +
-          (error.response && error.response.data
-            ? error.response.data.message
-            : error.message)
+            (error.response && error.response.data ? error.response.data.message : error.message)
         );
       }
     },
@@ -109,22 +91,17 @@ export default {
         return;
       }
       try {
-        await axios.delete(
-          `http://localhost:8080/question/delete?idx=${this.questionIdx}`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        await axios.delete(`http://localhost:8080/question/delete?idx=${this.questionIdx}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         alert("문의사항이 성공적으로 삭제되었습니다.");
-        this.$router.push('/question'); // 삭제 후 inquiry 목록으로 리다이렉트
+        this.$router.push("/question"); // 삭제 후 inquiry 목록으로 리다이렉트
       } catch (error) {
         alert(
           "문의사항 삭제 중 오류가 발생했습니다: " +
-          (error.response && error.response.data
-            ? error.response.data.message
-            : error.message)
+            (error.response && error.response.data ? error.response.data.message : error.message)
         );
       }
     },
@@ -135,7 +112,7 @@ export default {
     this.questionTitle = title;
     this.questionContent = content;
     this.questionCategory = category;
-    this.questionStatus = status === 'true';
+    this.questionStatus = status === "true";
     this.answerContent = answerContent; // answerContent를 초기화합니다.
   },
 };
