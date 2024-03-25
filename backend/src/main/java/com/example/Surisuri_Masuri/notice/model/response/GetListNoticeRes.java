e62ecc4e10cd251e98104be84ebeb88564700c27
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-25 16:41:33 +0900

    [Fix] 공지사항 목록 조회 응답 DTO 수정
    noticeIdx 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/notice/model/response/GetListNoticeRes.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/notice/model/response/GetListNoticeRes.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class GetListNoticeRes {
 
+    private Integer noticeIdx;
     private String category;
     private String title;
     private String content;
