2d5a09103573a27b62f9508baa0da3409235d676
ParkHyeonBeom <gusqja0213@gmail.com>
2024-03-28 00:02:20 +0900

    [Feat] User 관련 기능 구현을 위한 Login 관련 응답 Dto 수정
      - discardedProduct 속성 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ResDtos/LoginRes.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ResDtos/LoginRes.java
@@ -1,10 +1,14 @@
 package com.example.Surisuri_Masuri.member.Model.ResDtos;
 
+import com.example.Surisuri_Masuri.storeStock.Model.Entity.StoreStock;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 @Builder
 @Data
 public class LoginRes {
-    private final String jwtToken;
+    private String jwtToken;
+    private List<DiscardedProduct> discardedProduct;
 }
