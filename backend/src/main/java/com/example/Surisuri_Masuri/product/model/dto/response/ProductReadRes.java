ba582f7ede0a2f7dff2da808ee8dd93d652de0b6
ParkHyeonBeom <gusqja0213@naver.com>
2024-03-27 17:20:45 +0900

    [Feat] Product Category 기능 구현
      - 상품에 카테고리 속성 구현

--- backend/src/main/java/com/example/Surisuri_Masuri/product/model/dto/response/ProductReadRes.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/product/model/dto/response/ProductReadRes.java
@@ -1,5 +1,6 @@
 package com.example.Surisuri_Masuri.product.model.dto.response;
 
+import com.example.Surisuri_Masuri.product.model.productEnum.ProductCategory;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +10,5 @@ public class ProductReadRes {
     private Long productIdx;
     private String productName;
     private Integer price;
+    private ProductCategory productCategory;
 }
