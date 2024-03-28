59d1bdb3255447f62fde9bdec5aa645980312d44
jwj <llongkate@naver.com>
2024-03-28 17:43:36 +0900

    [Feat] 가맹점 관리자 회원 정보 예외 처리 추가
    가맹점 관리자가 회원 정보 수정을 할 때 나는 에러 코드를 수정하고 예외 처리 코드를 추가 하고 있었습니다.
    
    (#4)

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserUpdateReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserUpdateReq.java
@@ -16,6 +16,7 @@ public class UserUpdateReq {
     @Pattern(regexp = "^(\\d{2,3}-?\\d{3,4}-?\\d{4}|\\d{10,11}|)$", message = "올바른 전화번호 형식이어야 합니다.")
     private String userPhoneNo;
 
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대문자를 포함한 8~15자의 영문, 숫자, 특수문자(!@#$%^&*()_+-=[])만 가능합니다.")
     private String userPassword;
 
     public boolean isUserPasswordValid() {
