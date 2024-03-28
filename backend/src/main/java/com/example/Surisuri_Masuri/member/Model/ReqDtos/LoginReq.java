cb89e8f3f9fd7c3142c90fbb824cd3f69f41cf31
jwj <llongkate@naver.com>
2024-03-28 16:49:54 +0900

    [Feat] 가맹점 관리자 예외 처리 추가
    가맹점 관리자가 로그인 및 회원 가입을 할 때 입력값 검증 코드와 예외 처리 코드를 추가했습니다.
    
    (#1 #5)

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/LoginReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/LoginReq.java
@@ -10,8 +10,9 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class LoginReq {
 
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "올바른 이메일 형식(test@example.com)으로 입력해야 합니다.")
     String id;
 
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$",message = "잘못된 비밀번호")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$",message = "잘못된 비밀번호 형식입니다.")
     String password;
 }
--- backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserSignUpReq.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Model/ReqDtos/UserSignUpReq.java
@@ -11,10 +11,10 @@ import javax.validation.constraints.Pattern;
 @Data
 public class UserSignUpReq {
 
-    @Pattern(regexp = "^[가-힣]*$", message = "한글만 가능합니다.")
+    @Pattern(regexp = "^[가-힣]*$", message = "한글만 입력이 가능합니다.")
     private String userName;
 
-    @Email(message = "올바른 이메일 형식이어야 합니다.")
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$", message = "올바른 이메일 형식(test@example.com)으로 입력해야 합니다.")
     private String userEmail;
 
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 대문자를 포함한 8~15자의 영문, 숫자, 특수문자(!@#$%^&*()_+-=[])만 가능합니다.")
