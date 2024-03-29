4dbc3604676a76effcfa76de5c9382a459a7f8ca
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-29 09:55:00 +0900

    [Feat] ContainerStockRepositoryCustom 쿼리 추가
    특정 Container의 ContainerStock에 특정 상품의 유효기간을 찾는 쿼리 추가

--- backend/src/main/java/com/example/Surisuri_Masuri/container/repository/QueryDsl/ContainerStockRepositoryCustom.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/container/repository/QueryDsl/ContainerStockRepositoryCustom.java
@@ -1,10 +1,14 @@
 package com.example.Surisuri_Masuri.container.repository.QueryDsl;
 
-import com.example.Surisuri_Masuri.cart.model.CartDetail;
 import com.example.Surisuri_Masuri.container.model.entity.ContainerStock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ContainerStockRepositoryCustom {
     public Page<ContainerStock> findList(Integer containerIdx, Pageable pageable);
+
+    public List<LocalDate> findByContainerIdxAndProductName(Integer containerIdx, String productName);
 }
