ddb61a0d330f3bf3c523511d58df6a790248337d
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-03-29 10:05:26 +0900

    [Feat] Orders 레포지토리 쿼리DSL 기능 추가
    QueryDSL의 페이징 기능 적용하여 Orders 불러오는 쿼리 추가

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/orders/repository/QueryDsl/OrdersRepositoryCustomImpl.java
@@ -0,0 +1,30 @@
+package com.example.Surisuri_Masuri.orders.repository.QueryDsl;
+
+import com.example.Surisuri_Masuri.orders.model.Orders;
+import com.example.Surisuri_Masuri.orders.model.QOrders;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+public class OrdersRepositoryCustomImpl extends QuerydslRepositorySupport implements OrdersRepositoryCustom {
+
+    public OrdersRepositoryCustomImpl() {
+        super(Orders.class);
+    }
+    @Override
+    public Page<Orders> findList(Pageable pageable) {
+        QOrders orders = new QOrders("orders");
+
+        List<Orders> result = from(orders)
+                .distinct()
+                .offset(pageable.getPageNumber() * pageable.getPageSize())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(result, pageable, result.size());
+    }
+
+}
