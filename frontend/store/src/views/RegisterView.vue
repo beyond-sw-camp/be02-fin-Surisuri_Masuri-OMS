d1d06390296164548dc63e71216321a7ae1a36e9
DESKTOP-VFTL4HI\Playdata <zofnapf@naver.com>
2024-04-01 13:57:02 +0900

    [Fix] 회원가입 예외처리 수정
    성공시에도 실패알림 수정

--- frontend/store/src/views/RegisterView.vue
+++ frontend/store/src/views/RegisterView.vue
@@ -11,47 +11,99 @@
               <div class="card-body">
                 <form @submit.prevent="createAccount">
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.userName" class="form-control" id="inputName" type="text" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.userName"
+                      class="form-control"
+                      id="inputName"
+                      type="text"
+                      placeholder=""
+                    />
                     <label for="inputName">성함</label>
                   </div>
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.userEmail" class="form-control" id="inputEmail" type="email" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.userEmail"
+                      class="form-control"
+                      id="inputEmail"
+                      type="email"
+                      placeholder=""
+                    />
                     <label for="inputEmail">Email</label>
                   </div>
                   <div class="row mb-3">
                     <div class="col-md-6">
                       <div class="form-floating mb-3 mb-md-0">
-                        <input v-model="userSignUpReq.userPassword" class="form-control" id="inputPassword" type="password" placeholder="" />
+                        <input
+                          v-model="userSignUpReq.userPassword"
+                          class="form-control"
+                          id="inputPassword"
+                          type="password"
+                          placeholder=""
+                        />
                         <label for="inputPassword">비밀번호</label>
                       </div>
                     </div>
                     <div class="col-md-6">
                       <div class="form-floating mb-3 mb-md-0">
-                        <input v-model="confirmPassword" class="form-control" id="inputPasswordConfirm" type="password" placeholder="" />
-                        <label for="inputPasswordConfirm">비밀번호 재확인</label>
+                        <input
+                          v-model="confirmPassword"
+                          class="form-control"
+                          id="inputPasswordConfirm"
+                          type="password"
+                          placeholder=""
+                        />
+                        <label for="inputPasswordConfirm"
+                          >비밀번호 재확인</label
+                        >
                       </div>
                     </div>
                   </div>
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.userPhoneNo" class="form-control" id="inputPhoneNumber" type="text" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.userPhoneNo"
+                      class="form-control"
+                      id="inputPhoneNumber"
+                      type="text"
+                      placeholder=""
+                    />
                     <label for="inputPhoneNumber">핸드폰 번호</label>
                   </div>
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.storeUuid" class="form-control" id="inputStoreUuid" type="text" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.storeUuid"
+                      class="form-control"
+                      id="inputStoreUuid"
+                      type="text"
+                      placeholder=""
+                    />
                     <label for="inputStoreUuid">가맹점 UUID</label>
                   </div>
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.storeAddr" class="form-control" id="inputStoreAddress" type="text" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.storeAddr"
+                      class="form-control"
+                      id="inputStoreAddress"
+                      type="text"
+                      placeholder=""
+                    />
                     <label for="inputStoreAddress">가맹점 주소</label>
                   </div>
                   <div class="form-floating mb-3">
-                    <input v-model="userSignUpReq.storePhoneNo" class="form-control" id="inputStorePhoneNumber" type="text" placeholder="" />
+                    <input
+                      v-model="userSignUpReq.storePhoneNo"
+                      class="form-control"
+                      id="inputStorePhoneNumber"
+                      type="text"
+                      placeholder=""
+                    />
                     <label for="inputStorePhoneNumber">가맹점 전화번호</label>
                   </div>
-                  
+
                   <div class="mt-4 mb-0">
                     <div class="d-grid">
-                      <button type="submit" class="btn btn-primary btn-block">회원가입 하기</button>
+                      <button type="submit" class="btn btn-primary btn-block">
+                        회원가입 하기
+                      </button>
                     </div>
                   </div>
                 </form>
@@ -70,26 +122,28 @@
 </template>
 
 <script>
-import { ref } from 'vue';
-import { useUserStore } from '../../stores/userStore.js';
-import { getErrorMessage } from '../utils/error.js'; // 에러 처리 함수 임포트
+import { ref } from "vue";
+import { useUserStore } from "../../stores/userStore.js";
+import { getErrorMessage } from "../utils/error.js"; // 에러 처리 함수 임포트
+import { useRouter } from 'vue-router';
 
 export default {
-  name: 'RegisterView',
+  name: "RegisterView",
   setup() {
+    const router = useRouter();
     const userStore = useUserStore();
-    const confirmPassword = ref('');
+    const confirmPassword = ref("");
 
     const createAccount = async () => {
       if (userStore.userSignUpReq.userPassword !== confirmPassword.value) {
-        alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
         return;
       }
 
       const result = await userStore.createAccount();
       if (result === true) {
-        alert('회원가입이 성공적으로 완료되었습니다.');
-        // 회원가입 성공 후 로그인 페이지나 홈페이지로 리다이렉트 처리
+        alert("회원가입이 성공적으로 완료되었습니다.");
+        router.push('/login');
       } else {
         const errorMessage = getErrorMessage(result); // 에러 코드를 이용해 에러 메시지를 얻음
         alert(errorMessage); // 에러 메시지 표시
