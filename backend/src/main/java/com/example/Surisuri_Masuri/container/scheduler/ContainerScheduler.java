d2116b9ab8bbb0fed15683adefe82bb0c36fd69a
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-04-01 11:15:31 +0900

    [Fix] ContainerScheduler 수정
    스케줄러 실행 시간 변경

--- backend/src/main/java/com/example/Surisuri_Masuri/container/scheduler/ContainerScheduler.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/container/scheduler/ContainerScheduler.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class ContainerScheduler {
     private final ContainerSchedulingService containerSchedulingService;
 
-    @Scheduled(cron = "*/30 * 17 * * *\n") // 1초에 1000
+    @Scheduled(cron = "*/30 * 11 * * *") // 1초에 1000
     public void test() throws IOException {
         System.out.println("test");
         containerSchedulingService.discardExpiredFoodProducts();
