2dfb1cf2255f70e4d1bb78cf43c10f037b366afc
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-04-02 10:21:57 +0900

    [Fix] 유저 레포지토리 수정
    회원 이름 중복 가능성 존재하기 때문에 List로 받도록 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/member/Repository/UserRepository.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/member/Repository/UserRepository.java
@@ -5,13 +5,14 @@ import com.example.Surisuri_Masuri.member.Model.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUserEmail(String userEmail);
 
-    Optional<User> findByUserName(String userName);
+    List<User> findByUserName(String userName);
 
     Optional<User> findByUserPhoneNo(String userPhoneNo);
 
