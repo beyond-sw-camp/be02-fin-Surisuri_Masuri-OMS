e164c2fe428d3b9fafc58287c53e20020d228f21
ParkHyeonBeom <gusqja0213@naver.com>
2024-04-03 16:02:31 +0900

    [Feat] JwtFilter 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/jwt/JwtFilter.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/jwt/JwtFilter.java
@@ -1,12 +1,17 @@
 package com.example.Surisuri_Masuri.jwt;
 
+import com.example.Surisuri_Masuri.exception.EntityException.UserException;
+import com.example.Surisuri_Masuri.exception.ErrorCode;
+import com.example.Surisuri_Masuri.jwt.Model.RefreshToken;
+import com.example.Surisuri_Masuri.jwt.Repository.RefreshTokenRepository;
 import com.example.Surisuri_Masuri.member.Model.Entity.Manager;
 import com.example.Surisuri_Masuri.member.Model.Entity.User;
 import com.example.Surisuri_Masuri.member.Service.ManagerService;
 import com.example.Surisuri_Masuri.member.Service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import javax.servlet.FilterChain;
@@ -14,76 +19,146 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-
     private final UserService userService;
 
     private final ManagerService managerService;
 
+    private final RefreshTokenRepository refreshTokenRepository;
+
     // Spring Security를 통해 전달 받은 secretKey
     private final String secretKey;
+
+    private final JwtUtils jwtUtils;
+
+    @Value("${jwt.token.expired-time-ms}")
+    private int expiredTimeMs;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        // 요청에 대한 정보가 담긴 Request에서 Header를 추출
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String accessToken = JwtUtils.getHeaderToken(request, "Access");
+        String refreshToken = JwtUtils.getHeaderToken(request, "Refresh");
 
-        // 헤더에 담긴 JWT 토큰을 추출하기 위한 작업, Jwt 토큰은 Bearer로 시작하는 특징을 활용
-        String token;
-        if (header != null && header.startsWith("Bearer ")) {
-            token = header.split(" ")[1];
-        } else {
+
+        if (accessToken == null)
+        // 다음 필터로 넘어간다
+        {
             filterChain.doFilter(request, response);
             return;
-        }
+        } else if (accessToken != null && refreshToken == null) {
 
-        // 이후 사용자를 구별하기위해 Email을 통해 사용자 탐색
-        String email = JwtUtils.getUserEmail(token, secretKey);
-        Long idx = JwtUtils.getUserIdx(token, secretKey);
-        String managerId = JwtUtils.getManagerInfo(token,secretKey);
+            String email = null;
+            String managerId = null;
 
-        // 가맹점 먼저 확인
-        User user = userService.getUserByUserEmail(email);
+            if (!JwtUtils.validateAccessToken(accessToken, secretKey)) {
 
-        if (user != null) {
-            String userEmail = user.getUserEmail();
+                email = JwtUtils.getUserId(accessToken, secretKey);
+                managerId = JwtUtils.getManagerId(accessToken, secretKey);
 
-            if (!JwtUtils.validate(token, userEmail, secretKey)) {
-                filterChain.doFilter(request, response);
-                return;
+
+                User user = userService.getUserByUserEmail(email);
+                Manager manager = managerService.getManagerByManagerId(managerId);
+
+                if (user != null) {
+                    setUserAuthentication(JwtUtils.getUserEmail(accessToken, secretKey));
+                    filterChain.doFilter(request, response);
+                    return;
+
+                } else if (manager != null) {
+                    setManagerAuthentication(JwtUtils.getManagerInfo(accessToken, secretKey));
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+
             }
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    User.builder().userEmail(email).idx(idx).build(), null,
-                    user.getAuthorities()
-            );
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-
-            filterChain.doFilter(request, response);
-        }
-
-        // 가맹점이 아니면 본사직원일것이다
-        else {
-            Manager manager = managerService.getManagerByManagerId(managerId);
-
-            String managerId2 = manager.getUsername();
-
-            if (!JwtUtils.validate2(token, managerId2, secretKey)) {
-                filterChain.doFilter(request, response);
-                return;
+            else {
+                    // 토큰이 만료되었거나 유효하지 않은 경우
+                    try {
+                        throw new UserException(ErrorCode.INVALID_TOKEN, "만료된 토큰입니다");
+                    } catch (UserException e) {
+                        // 여기서 적절한 에러 처리를 수행합니다.
+                        // 예: 사용자에게 에러 메시지를 JSON 형태로 응답하기
+                        // 응답 상태 코드 설정 (예: HttpServletResponse.SC_UNAUTHORIZED)
+                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        response.setContentType("application/json");
+                        response.setCharacterEncoding("UTF-8");
+                        response.getWriter().write("{\n    \"error\": \"" + e.getMessage() + "\",\n    \"code\": \"" + (ErrorCode.INVALID_TOKEN.getCode()) + "\"\n}");
+                    }
             }
-            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                    manager, null,
-                    manager.getAuthorities()
-            );
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else if (accessToken != null && refreshToken != null) {
 
-            filterChain.doFilter(request, response);
+            String email = null;
+            String managerId = null;
+            // 만약 액세스 토큰이 유효하지 않는다면
+            if (JwtUtils.validateAccessToken(accessToken, secretKey))
+            // 해당 액세스 토큰을 통해 사용자의 정보를 조회하고
+            // 조회된 사용자의 정보를 통해 리프레쉬 토큰을 찾는다
+            // 만약 찾은 리프레시 토큰과 전달 받은 리프레쉬 토큰이 같고 && 아직 유효한 토큰이라면
+            // 새로운 액세스 토큰을 발급해준다 아니라면 로그아웃 신호를 보내 강제로 로그아웃 시키고 리프레시 토큰을 삭제한다.
+            {
+                email = JwtUtils.getUserId(accessToken, secretKey);
+                managerId = JwtUtils.getManagerId(accessToken, secretKey);
+
+                if (email != null) {
+                    Optional<RefreshToken> refreshTokenById = refreshTokenRepository.findByUserId(email);
+                    if (refreshTokenById != null && refreshTokenById.get().getRefreshToken().equals(refreshToken) &&
+                            !JwtUtils.validateRefreshToken(refreshToken, secretKey)) {
+                        filterChain.doFilter(request, response);
+                        return;
+                    } else {
+                        // 토큰이 만료되었거나 유효하지 않은 경우
+                        try {
+                            refreshTokenRepository.deleteByUserId(email);
+                            throw new UserException(ErrorCode.INVALID_TOKEN, "만료된 토큰입니다");
+                        } catch (UserException e) {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+                            response.setCharacterEncoding("UTF-8");
+                            response.getWriter().write("{\n    \"error\": \"" + e.getMessage() + "\",\n    \"code\": \"" + e.getErrorCode() + "\"\n}");
+                        }
+                    }
+                } else if (managerId != null) {
+                    Optional<RefreshToken> refreshTokenById = refreshTokenRepository.findByUserId(managerId);
+                    if (refreshTokenById != null && refreshTokenById.get().getRefreshToken().equals(refreshToken) &&
+                            !JwtUtils.validateRefreshToken(refreshToken, secretKey)) {
+                        filterChain.doFilter(request, response);
+                        return;
+                    } else {
+                        // 토큰이 만료되었거나 유효하지 않은 경우
+                        try {
+                            refreshTokenRepository.deleteByUserId(managerId);
+                            throw new UserException(ErrorCode.INVALID_TOKEN, "만료된 토큰입니다");
+                        } catch (UserException e) {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json");
+                            response.setCharacterEncoding("UTF-8");
+                            response.getWriter().write("{\n    \"error\": \"" + e.getMessage() + "\",\n    \"code\": \"" + e.getErrorCode() + "\"\n}");
+                        }
+                    }
+                }
+            }
         }
+    }
+    public void setManagerAuthentication(String id) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtUtils.createManagerAuthentication(id);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    public void setUserAuthentication(String id) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = jwtUtils.createUserAuthentication(id);
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
