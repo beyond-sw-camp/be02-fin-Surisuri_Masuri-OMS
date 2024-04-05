2c35b3c7f8179f9f2f1b7f0c00935a2f7e0fa4bf
ParkHyeonBeom <gusqja0213@naver.com>
2024-04-05 10:27:39 +0900

    [Fix] Redis 적용에 따른 RefreshToken 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/jwt/Model/RefreshToken.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/jwt/Model/RefreshToken.java
@@ -2,31 +2,29 @@ package com.example.Surisuri_Masuri.jwt.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
-@Entity
-@NoArgsConstructor
+
 @AllArgsConstructor
 @Builder
+@RedisHash(value = "refreshToken", timeToLive = 1800)
 public class RefreshToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    @Indexed
     private String userId;
+
     private String refreshToken;
 
     public RefreshToken(String userId, String refreshToken) {
         this.userId = userId;
         this.refreshToken = refreshToken;
     }
-
 }
