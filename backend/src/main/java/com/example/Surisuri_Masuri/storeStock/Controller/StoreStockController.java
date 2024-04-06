64e6cdf51f993c9394eac39aefc549727631a96c
ParkHyeonBeom <gusqja0213@naver.com>
2024-04-06 20:53:54 +0900

    [Fix] Redis 적용에 따른 RequsetHeader 반영

--- backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Controller/StoreStockController.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Controller/StoreStockController.java
@@ -18,39 +18,34 @@ public class StoreStockController {
 
     // 재고 등록
     @PostMapping("/stock/create")
-    public ResponseEntity storeStockCreate(@RequestHeader(value = "Authorization") String token, @RequestBody StoreStockCreateReq storeStockCreateReq) {
+    public ResponseEntity storeStockCreate(@RequestHeader(value = "AccessToken") String token, @RequestBody StoreStockCreateReq storeStockCreateReq) {
         return ResponseEntity.ok().body(storeStockService.storeStockCreate(token, storeStockCreateReq));
     }
 
 
     // 전체 재고 조회
     @RequestMapping(method = RequestMethod.GET, value = "/stock/list")
-    public ResponseEntity storeStockList(@RequestHeader(value = "Authorization") String token, Integer page, Integer size) {
+    public ResponseEntity storeStockList(@RequestHeader(value = "AccessToken") String token, Integer page, Integer size) {
         return ResponseEntity.ok().body(storeStockService.storeStockList(token, page, size));
     }
 
     // 단일 재고 조회
     @RequestMapping(method = RequestMethod.GET, value = "/stock/search")
-    public ResponseEntity storeStockSearch(@RequestHeader(value = "Authorization") String token, @RequestBody StoreStockSearchReq storeStockSearchReq) {
+    public ResponseEntity storeStockSearch(@RequestHeader(value = "AccessToken") String token, @RequestBody StoreStockSearchReq storeStockSearchReq) {
         return ResponseEntity.ok().body(storeStockService.storeStockSearch(token, storeStockSearchReq));
     }
 
     // 재고 수정
     @PatchMapping("/stock/update")
-    public ResponseEntity storeStockUpdate(@RequestHeader(value = "Authorization") String token, @RequestBody StoreStockUpdateReq storeStockUpdateReq) {
+    public ResponseEntity storeStockUpdate(@RequestHeader(value = "AccessToken") String token, @RequestBody StoreStockUpdateReq storeStockUpdateReq) {
         return ResponseEntity.ok().body(storeStockService.storeStockUpdate(token, storeStockUpdateReq));
     }
 
 
     // 재고 삭제
     @DeleteMapping ("/stock/delete")
-    public ResponseEntity storeStockDelete(@RequestHeader(value = "Authorization") String token, @RequestBody StoreStockDeleteReq storeStockDeleteReq) {
+    public ResponseEntity storeStockDelete(@RequestHeader(value = "AccessToken") String token, @RequestBody StoreStockDeleteReq storeStockDeleteReq) {
         return ResponseEntity.ok().body(storeStockService.storeStockDelete(token, storeStockDeleteReq));
-    }
-
-    @GetMapping("/nearExpiredFoodStocks")
-    public ResponseEntity<BaseResponse<List<ExpiredFoodStockDto>>> findNearExpiredFoodStocks(@RequestHeader("Authorization") String token) {
-        return ResponseEntity.ok().body(storeStockService.findExpiringFoodStocks(token));
     }
 
 }
