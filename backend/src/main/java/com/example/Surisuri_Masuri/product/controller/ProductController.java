b137becd54c9dfbe02277a6b692ac030b64218e7
ParkHyeonBeom <gusqja0213@naver.com>
2024-04-06 20:54:03 +0900

    [Fix] Redis 적용에 따른 RequsetHeader 반영

--- backend/src/main/java/com/example/Surisuri_Masuri/product/controller/ProductController.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/product/controller/ProductController.java
@@ -12,33 +12,36 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class ProductController {
+
     private final ProductService productService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/create")
-    public ResponseEntity create(@RequestBody ProductCreateReq req) {
-        return ResponseEntity.ok().body(productService.create(req));
+    public ResponseEntity create(@RequestHeader(value = "AccessToken") String token,@RequestBody ProductCreateReq req) {
+        return ResponseEntity.ok().body(productService.create(token,req));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/search")
-    public ResponseEntity search(@RequestParam String productName,
+    public ResponseEntity search(@RequestHeader(value = "AccessToken") String token,
+                                 @RequestParam String productName,
                                  @RequestParam(required = false, defaultValue = "1") Integer page,
                                  @RequestParam(required = false, defaultValue = "10") Integer size) {
-        return ResponseEntity.ok().body(productService.search(productName,page,size));
+        return ResponseEntity.ok().body(productService.search(token,productName,page,size));
     }
 
     @GetMapping("/list")
-    public ResponseEntity list(@RequestParam(required = false, defaultValue = "1") Integer page,
+    public ResponseEntity list(@RequestHeader(value = "AccessToken") String token,
+                               @RequestParam(required = false, defaultValue = "1") Integer page,
                                @RequestParam(required = false, defaultValue = "10") Integer size) {
-        return ResponseEntity.ok().body(productService.list(page, size));
+        return ResponseEntity.ok().body(productService.list(token,page, size));
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/update")
-    public ResponseEntity update(@RequestBody ProductUpdateReq req) {
-        return ResponseEntity.ok().body(productService.update(req));
+    public ResponseEntity update(@RequestHeader(value = "AccessToken") String token,@RequestBody ProductUpdateReq req) {
+        return ResponseEntity.ok().body(productService.update(token,req));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete")
-    public ResponseEntity delete(Long idx) {
-        return ResponseEntity.ok().body(productService.delete(idx));
+    public ResponseEntity delete(@RequestHeader(value = "AccessToken") String token,Long idx) {
+        return ResponseEntity.ok().body(productService.delete(token,idx));
     }
 }
