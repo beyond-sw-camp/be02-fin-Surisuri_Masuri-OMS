5d39452ed51cae540ce85efa7b0dbc3374d4dda8
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-04-09 00:55:57 +0900

    [Fix] 주문 기능 컨트롤러 수정
    @RequestHeader 이용해서 Authorization 토큰 받아오도록 코드 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/orders/controller/OrdersController.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/orders/controller/OrdersController.java
@@ -2,14 +2,11 @@ package com.example.Surisuri_Masuri.orders.controller;
 
 import com.example.Surisuri_Masuri.exception.EntityException.UserException;
 import com.example.Surisuri_Masuri.exception.ErrorCode;
-import com.example.Surisuri_Masuri.member.Model.Entity.Manager;
-import com.example.Surisuri_Masuri.member.Model.Entity.User;
 import com.example.Surisuri_Masuri.orders.model.dto.request.OrdersRefundReq;
 import com.example.Surisuri_Masuri.orders.model.dto.request.OrdersUpdateDeliveryReq;
 import com.example.Surisuri_Masuri.orders.service.OrdersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,14 +28,14 @@ public class OrdersController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/refund")
-    public ResponseEntity refund(OrdersRefundReq req) {
+    public ResponseEntity refund(@RequestHeader(value = "AccessToken") String token, @RequestBody OrdersRefundReq req) {
         try {
             return ResponseEntity.ok().body(ordersService.refundRequest(req));
         } catch (Exception e) {
             e.printStackTrace();
         }
         throw new UserException(ErrorCode.OrdersPayment_002,
-                String.format("결제를 실패했습니다."));
+                String.format("결제 취소를 실패했습니다."));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/list")
@@ -48,21 +45,21 @@ public class OrdersController {
     }
 
     @RequestMapping(method = RequestMethod.PATCH, value = "/updatedelivery")
-    public ResponseEntity updateOrdersDelivery(@RequestBody OrdersUpdateDeliveryReq req) {
+    public ResponseEntity updateOrdersDelivery(@RequestHeader(value = "Authorization") String token, @RequestBody OrdersUpdateDeliveryReq req) {
 
         return ResponseEntity.ok().body(ordersService.updateOrdersDelivery(req));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/deliverystatus")
-    public ResponseEntity showDeliveryStatus(Long ordersIdx) {
+    public ResponseEntity showDeliveryStatus(@RequestHeader(value = "Authorization") String token, Long ordersIdx) {
 
         return ResponseEntity.ok().body(ordersService.showDeliveryStatus(ordersIdx));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{merchantUid}")
     public ResponseEntity listDetailByMerchantUid(@PathVariable String merchantUid,
-                                                  @AuthenticationPrincipal Manager manager) {
+                                                  @RequestHeader(value = "Authorization") String token) {
 
-        return ResponseEntity.ok().body(ordersService.listDetailByMerchantUid(merchantUid, manager));
+        return ResponseEntity.ok().body(ordersService.listDetailByMerchantUid(merchantUid, token));
     }
 }
