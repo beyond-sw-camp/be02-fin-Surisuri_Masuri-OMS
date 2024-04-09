88a806400e55f21ee1a285eaac6ea53ecbdf46a7
ParkHyeonBeom <gusqja0213@naver.com>
2024-04-09 18:09:58 +0900

    [Fix]  deleteStoreStockByStockIdxAndStoreUuid 수정
      - mariaDB에서 지원하지 않는 Cross Join 관련 동작으로 발생하는 에러 수정

--- backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Repository/QueryDsl/StoreStockRepositoryCustomImpl.java
+++ backend/src/main/java/com/example/Surisuri_Masuri/storeStock/Repository/QueryDsl/StoreStockRepositoryCustomImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,9 +20,12 @@ public class StoreStockRepositoryCustomImpl extends QuerydslRepositorySupport im
 
     private final JPAQueryFactory queryFactory;
 
-    public StoreStockRepositoryCustomImpl(JPAQueryFactory queryFactory) {
+    private final EntityManager em;
+
+    public StoreStockRepositoryCustomImpl(JPAQueryFactory queryFactory, EntityManager em) {
         super(StoreStock.class);
         this.queryFactory = queryFactory;
+        this.em = em;
     }
 
     @Override
@@ -41,16 +45,18 @@ public class StoreStockRepositoryCustomImpl extends QuerydslRepositorySupport im
 
     @Override
     @Transactional
-    public Long deleteStoreStockByProductIdxAndStoreUuid(Long productIdx, String storeUuid) {
-        QStoreStock storeStock = new QStoreStock("storeStock");
-        // 실제 삭제 수행
-        // 실제 삭제 수행
-        Long deletedCount = queryFactory
-                .delete(storeStock)
-                .where(storeStock.product.idx.eq(productIdx)
-                        .and(storeStock.store.storeUuid.eq(storeUuid)))
-                .execute();
-
-        return deletedCount; // 삭제된 레코드 수 반환
+    public Long deleteStoreStockByStockIdxAndStoreUuid(Long stockIdx, String storeUuid) {
+        // Step 1: 조건에 맞는 StoreStock 조회
+        List<StoreStock> storeStocks = queryFactory
+                .selectFrom(QStoreStock.storeStock)
+                .where(QStoreStock.storeStock.idx.eq(stockIdx)
+                        .and(QStoreStock.storeStock.store.storeUuid.eq(storeUuid)))
+                .fetch();
+        // Step 2: 조회된 StoreStock 삭제
+        for (StoreStock storeStock : storeStocks) {
+            em.remove(storeStock); // 'em'은 EntityManager의 인스턴스
+        }
+        // 삭제된 레코드 수 반환
+        return (long) storeStocks.size();
     }
 }
