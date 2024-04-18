17cafe54e8bd1d23e3765b8e620c9eedbdfff45f
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-04-18 14:33:17 +0900

    [Feat] Test
    무중단 배포 확인을 위한 메서드 추가

---
+++ backend/src/main/java/com/example/Surisuri_Masuri/common/Test.java
@@ -0,0 +1,21 @@
+package com.example.Surisuri_Masuri.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
+public class Test {
+
+    // 계정 생성 - Create
+    @GetMapping("/test")
+    public ResponseEntity test()
+    {
+        return ResponseEntity.ok().body("v1");
+    }
+
+}
